421641323a44076342e963675f0b18771670deff
Elaine Natalia Mendes Menino <87189331+natymendes@users.noreply.github.com>
2024-11-04 09:42:51 -0300

    Create example.php

---
+++ example.php
@@ -0,0 +1,11 @@
+
+Este arquivo contém um exemplo prático de uso de OOP no PHP.
+
+```php
+<?php
+
+class Produto {
+    public $nome;
+    public $preco;
+
+    public function __construct($nome, $preco) {
